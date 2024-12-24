c02fc1fba2c8e751a6d07c70d8cb1b84d02f239b
fatima4242 <fathimareihab42@gmail.com>
2024-12-24 12:06:28 +0300

    Initial Commit

---
+++ visitor-count.js
@@ -0,0 +1,7 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://ev4gnx6jrsznaj7peokdlx5c3e0evepk.lambda-url.us-east-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = `Views: ${data}`;
+}
+updateCounter();
