84a9e9abd227a5235771350c7b5e007d36783f5a
fatima4242 <fathimareihab42@gmail.com>
2024-12-30 16:41:56 +0300

    Updated visitor-count.js

--- website/visitor-count.js
+++ website/visitor-count.js
@@ -2,6 +2,6 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://ev4gnx6jrsznaj7peokdlx5c3e0evepk.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = `Views: ${data}`;
+    counter.innerHTML = `Visitor Count: ${data}`;
 }
 updateCounter();
